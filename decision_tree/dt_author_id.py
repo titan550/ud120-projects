5a86dbe3dd732f01f9bdfbd41498b3540baa782e
titan_550_M <titan.550@gmail.com>
2016-07-23 08:33:11 -0700

    Decision Tree Homework Completed

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -14,6 +14,8 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
+
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -24,6 +26,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test, pred)
+print acc
 
 
 #########################################################
